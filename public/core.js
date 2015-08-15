2701cbb6239fa2a9f2ef591ce8b7d42000b5bcbd
mikefab <mikefabrikant@gmail.com>
2015-08-15 19:39:04 -0400

    First commit

---
+++ public/core.js
@@ -0,0 +1,26 @@
+var app = angular.module("cosmic", ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/home');
+    
+    $stateProvider
+        
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/home',
+            templateUrl: 'cosmic_partial.html'
+        })
+        
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('about', {
+            // we'll get to this in a bit       
+        });
+        
+});
+
+
+app.controller('mainCtrl', ['$scope', '$http', function($scope, $http) {
+  $scope.greeting = 'Hola!';
+
+}]);
